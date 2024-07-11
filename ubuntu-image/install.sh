fcd66be32963b4bdaaa066f194ff2f4c048c88ea
Sungchan Yi <calofmijuck@snu.ac.kr>
2024-07-11 11:12:26 +0900

    feat: installation script

---
+++ ubuntu-image/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+sudo apt update -y && apt upgrade -y
+sudo apt install -y git curl vim zsh net-tools zsh-syntax-highlighting
+
+# oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+rm .zshrc
+wget https://raw.githubusercontent.com/calofmijuck/macbuntu/master/ubuntu-image/.zshrc
+
+rm .vimrc
+wget https://github.com/calofmijuck/macbuntu/blob/master/ubuntu-image/.vimrc
+
+wget https://github.com/calofmijuck/macbuntu/blob/master/ubuntu-image/.tmux.conf
+
+echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+echo "export SHELL=/bin/zsh" >> .bash_profile
+echo "exec /bin/zsh -l" >> .bash_profile
